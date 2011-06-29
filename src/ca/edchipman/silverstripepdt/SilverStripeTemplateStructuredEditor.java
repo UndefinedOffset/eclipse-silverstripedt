b58152d5c198110c653c1195ca6a6d2e5b14130f
Ed Chipman <edchipman@gmail.com>
2011-06-29 00:51:58 +0000

    added ignore for bin contents

--- src/ca/edchipman/silverstripepdt/SilverStripeTemplateStructuredEditor.java
+++
@@ -1,8 +0,0 @@
-package ca.edchipman.silverstripepdt;
-
-import org.eclipse.php.internal.ui.editor.PHPStructuredEditor;
-
-@SuppressWarnings("restriction")
-public class SilverStripeTemplateStructuredEditor extends PHPStructuredEditor {
-    
-}
