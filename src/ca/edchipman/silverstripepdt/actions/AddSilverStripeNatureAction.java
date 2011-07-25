cb256a8048e53013c7fa2cc0f515d8c09245d325
Ed Chipman <edchipman@gmail.com>
2011-07-25 16:55:02 +0000

    started work on add build path entry

--- src/ca/edchipman/silverstripepdt/actions/AddSilverStripeNatureAction.java
+++ src/ca/edchipman/silverstripepdt/actions/AddSilverStripeNatureAction.java
@@ -1,8 +1,20 @@
 package ca.edchipman.silverstripepdt.actions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.ProgressMonitor;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.dltk.core.DLTKCore;
+import org.eclipse.dltk.core.IBuildpathEntry;
+import org.eclipse.dltk.internal.core.ScriptProject;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
@@ -26,8 +38,12 @@ public class AddSilverStripeNatureAction implements IObjectActionDelegate {
 	}
 
     public void run(IAction action) {
-		if(selProj == null)
+		try {
+			if(selProj == null || selProj.getNature("org.eclipse.php.core.PHPNature")==null)
+				return;
+		}catch (CoreException e1) {
 			return;
+		}
 		
 		try {
 			IProjectDescription description = selProj.getProject().getDescription();
@@ -47,6 +63,56 @@ public class AddSilverStripeNatureAction implements IObjectActionDelegate {
             if(CorePreferencesSupport.getInstance().getPreferencesValue(Keys.EDITOR_USE_SHORT_TAGS, null, null)=="true") {
                 CorePreferencesSupport.getInstance().setProjectSpecificPreferencesValue(Keys.EDITOR_USE_SHORT_TAGS, "false", selProj.getProject());
             }
+            
+            ScriptProject project=new ScriptProject(selProj, null);
+            
+            IBuildpathEntry[] rawBuildPath = project.getRawBuildpath();
+            IBuildpathEntry ssBuildPath=DLTKCore.newContainerEntry(new Path("ca.edchipman.silverstripepdt.LANGUAGE"));
+            boolean entryFound=false;
+            for(int i=0;i<rawBuildPath.length;i++) {
+            	if(rawBuildPath[i].equals(ssBuildPath)) {
+            		entryFound=true;
+            		break;
+            	}
+            }
+            
+            if(entryFound) {
+                List<IBuildpathEntry> buildPath = new ArrayList<IBuildpathEntry>();
+                buildPath.addAll(Arrays.asList(rawBuildPath));
+            	buildPath.add(ssBuildPath);
+            	
+            	IProgressMonitor monitor = new IProgressMonitor() {
+					@Override
+					public void worked(int work) {}
+					
+					@Override
+					public void subTask(String name) {}
+					
+					@Override
+					public void setTaskName(String name) {}
+					
+					@Override
+					public void setCanceled(boolean value) {}
+					
+					@Override
+					public boolean isCanceled() {
+						return false;
+					}
+					
+					@Override
+					public void internalWorked(double work) {}
+					
+					@Override
+					public void done() {}
+					
+					@Override
+					public void beginTask(String name, int totalWork) {}
+				};
+				
+            	project.setRawBuildpath(buildPath.toArray(rawBuildPath), monitor); //TODO not working
+            }
+            
+            CorePreferencesSupport.getInstance().setProjectSpecificPreferencesValue("silverstripe_version", "SS2.4", selProj);
 		}catch (Exception e) {
 			MessageDialog.openInformation(
 			         this.part.getSite().getShell(),
