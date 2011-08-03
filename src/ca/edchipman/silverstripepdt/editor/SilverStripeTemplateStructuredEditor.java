1ae4ba97fac90a3fdd35f4ed2b1b53c563e3b858
Ed Chipman <edchipman@gmail.com>
2011-08-03 01:22:03 +0000

    fixed content type

--- src/ca/edchipman/silverstripepdt/editor/SilverStripeTemplateStructuredEditor.java
+++ src/ca/edchipman/silverstripepdt/editor/SilverStripeTemplateStructuredEditor.java
@@ -1,8 +1,14 @@
 package ca.edchipman.silverstripepdt.editor;
 
+import org.eclipse.osgi.framework.debug.Debug;
+import org.eclipse.php.internal.core.preferences.CorePreferencesSupport;
+import org.eclipse.php.internal.core.preferences.CorePreferenceConstants.Keys;
 import org.eclipse.php.internal.ui.editor.PHPStructuredEditor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
 
 @SuppressWarnings("restriction")
 public class SilverStripeTemplateStructuredEditor extends PHPStructuredEditor {
-    
+    //@TODO disable asp tags some how
 }
