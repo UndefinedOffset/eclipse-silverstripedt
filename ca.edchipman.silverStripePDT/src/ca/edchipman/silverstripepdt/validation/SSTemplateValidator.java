e403a20c3bdb4f6164a6c3f288a0ae31d7b653cf
UndefinedOffset <echipman@webbuildersgroup.com>
2014-02-25 14:02:48 -0400

    Fixed class not found error on the template validator fixes #5

--- ca.edchipman.silverStripePDT/src/ca/edchipman/silverstripepdt/validation/SSTemplateValidator.java
+++ ca.edchipman.silverStripePDT/src/ca/edchipman/silverstripepdt/validation/SSTemplateValidator.java
@@ -1,6 +1,6 @@
 package ca.edchipman.silverstripepdt.validation;
 
-import org.eclipse.wst.html.internal.validation.HTMLValidator;
+import org.eclipse.wst.html.core.internal.validation.HTMLValidator;
 
 public class SSTemplateValidator extends HTMLValidator {
     
