33bfe30d77121d61a1efdad979792c1d2a7839fb
UndefinedOffset <echipman@webbuildersgroup.com>
2015-03-26 11:18:14 -0300

    Rough beginings of the extra plugins for silverstripe versions

--- ca.edchipman.silverStripePDT/src/ca/edchipman/silverstripepdt/SilverStripeVersion.java
+++ ca.edchipman.silverStripePDT/src/ca/edchipman/silverstripepdt/SilverStripeVersion.java
@@ -1,5 +1,9 @@
 package ca.edchipman.silverstripepdt;
 
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.e4.core.di.annotations.Execute;
+
 public class SilverStripeVersion {
     public static final String SS3_1="SS3.1";
     public static final String SS3_0="SS3.0";
@@ -7,4 +11,12 @@ public class SilverStripeVersion {
     public static final String SS2_3="SS2.3";
     public static final String FRAMEWORK_ONLY="framework";
     public static final String FULL_CMS="cms";
+    public static final String VERSION_EXTENSION_ID="ca.edchipman.silverStripePDT.ss_version";
+    
+    private static IConfigurationElement[] lang_registry;
+    
+    @Execute
+    public void execute(IExtensionRegistry registry) {
+        SilverStripeVersion.lang_registry=registry.getConfigurationElementsFor(VERSION_EXTENSION_ID);
+    }
 }
