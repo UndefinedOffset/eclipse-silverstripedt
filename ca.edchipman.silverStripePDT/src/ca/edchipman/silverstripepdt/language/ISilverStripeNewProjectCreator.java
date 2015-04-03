92be18928901d006ab75facaa3dd002f00670599
Ed Chipman <edchipman@gmail.com>
2015-04-03 18:12:36 -0300

    Started work on the project creator for 3.1
    Removed some duplicate required bundles

--- ca.edchipman.silverStripePDT/src/ca/edchipman/silverstripepdt/language/ISilverStripeNewProjectCreator.java
+++ ca.edchipman.silverStripePDT/src/ca/edchipman/silverstripepdt/language/ISilverStripeNewProjectCreator.java
@@ -1,8 +1,11 @@
 package ca.edchipman.silverstripepdt.language;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.text.templates.ContextTypeRegistry;
+import org.eclipse.jface.text.templates.persistence.TemplateStore;
+import org.eclipse.jface.wizard.Wizard;
 
 /**
  * Interface used in SilverStripe version plugins to define the project creation handlers 
@@ -15,8 +18,9 @@ public interface ISilverStripeNewProjectCreator {
      * @param monitor Monitor to update when creating the layout
      * @param templateRegistry Template registry to look through
      * @param isFrameworkLayout If the project is a framework only project this is set to true
+     * @throws CoreException 
      */
-    public void createProjectLayout(IProject project, IProgressMonitor monitor, ContextTypeRegistry templateRegistry, boolean isFrameworkLayout);
+    public void createProjectLayout(Wizard wizard, IProject project, IProgressMonitor monitor, ContextTypeRegistry templateRegistry, TemplateStore templateStore, boolean isFrameworkLayout) throws CoreException;
     
     /**
      * Performs the SilverStripe version specific tasks when creating new module layout project
@@ -24,8 +28,9 @@ public interface ISilverStripeNewProjectCreator {
      * @param monitor Monitor to update when creating the layout
      * @param templateRegistry Template registry to look through
      * @param isFrameworkLayout If the project is a framework only project this is set to true
+     * @throws CoreException 
      */
-    public void createModuleLayout(IProject project, IProgressMonitor monitor, ContextTypeRegistry templateRegistry, boolean isFrameworkLayout);
+    public void createModuleLayout(Wizard wizard, IProject project, IProgressMonitor monitor, ContextTypeRegistry templateRegistry, TemplateStore templateStore, boolean isFrameworkLayout) throws CoreException;
     
     /**
      * Performs the SilverStripe version specific tasks when creating new theme layout project
@@ -33,6 +38,7 @@ public interface ISilverStripeNewProjectCreator {
      * @param monitor Monitor to update when creating the layout
      * @param templateRegistry Template registry to look through
      * @param isFrameworkLayout If the project is a framework only project this is set to true
+     * @throws CoreException 
      */
-    public void createThemeLayout(IProject project, IProgressMonitor monitor, ContextTypeRegistry templateRegistry, boolean isFrameworkLayout);
+    public void createThemeLayout(Wizard wizard, IProject project, IProgressMonitor monitor, ContextTypeRegistry templateRegistry, TemplateStore templateStore, boolean isFrameworkLayout) throws CoreException;
 }
