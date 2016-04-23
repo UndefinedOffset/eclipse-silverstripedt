232e67f94f066bf339c902fed8466a3c299f1564
Ed Chipman <edchipman@gmail.com>
2016-04-23 16:37:29 -0300

    Added SilverStripePreferences class (missed in c92d3eba51897c00f5d3770ccf212a9244697ab0)

---
+++ ca.edchipman.silverStripePDT/src/ca/edchipman/silverstripepdt/preferences/SilverStripePreferences.java
@@ -0,0 +1,8 @@
+package ca.edchipman.silverstripepdt.preferences;
+
+public class SilverStripePreferences {
+    public static final String SILVERSTRIPE_VERSION = "silverstripe_version";
+    public static final String SILVERSTRIPE_FRAMEWORK_MODEL = "silverstripe_framework_model";
+    public static final String SILVERSTRIPE_SITECONFIG_MODULE = "silverstripe_siteconfig_module";
+    public static final String SILVERSTRIPE_REPORTS_MODULE = "silverstripe_reports_module";
+}
