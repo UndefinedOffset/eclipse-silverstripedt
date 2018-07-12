87ae41854487815fa08005477c66f38cb270d8ac
UndefinedOffset <echipman@webbuildersgroup.com>
2018-07-12 18:18:52 -0300

    Updated editor config and switched the validator

--- ca.edchipman.silverStripePDT/src/ca/edchipman/silverstripepdt/validation/SSTemplateValidator.java
+++ ca.edchipman.silverStripePDT/src/ca/edchipman/silverstripepdt/validation/SSTemplateValidator.java
@@ -1,8 +1,8 @@
 package ca.edchipman.silverstripepdt.validation;
 
-import org.eclipse.wst.html.core.internal.validation.HTMLValidator;
+import org.eclipse.wst.html.internal.validation.HTMLSourceValidator;
 
 @SuppressWarnings("restriction")
-public class SSTemplateValidator extends HTMLValidator {
+public class SSTemplateValidator extends HTMLSourceValidator {
     
 }
